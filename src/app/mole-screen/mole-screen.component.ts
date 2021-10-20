b1afe88cf6ad4b5a5c4a705a93a0fe5d04dd215c
Bongo9911 <dawsonbuist@gmail.com>
2021-10-20 19:09:44 -0400

    got the demo working

--- src/app/mole-screen/mole-screen.component.ts
+++ src/app/mole-screen/mole-screen.component.ts
@@ -10,10 +10,13 @@ import { initializeApp } from "firebase/app"
 export class MoleScreenComponent implements OnInit {
 
   unsubscribe: Unsubscribe[] = [];
+  xcoord: number = 0;
+  ycoord: number = 0;
   x: string = "0px";
   y: string = "0px";
   whack: boolean = false;
   moles: boolean[] = [false, false, false, false, false, false];
+  score: number = 0;
 
   constructor() { 
     const firebaseConfig = {
@@ -32,16 +35,55 @@ export class MoleScreenComponent implements OnInit {
     this.unsubscribe.push(onSnapshot(doc(db, "MoleGames", "Bongo"), (doc) => {
       console.log(doc.data());
       let data = doc.data() as {x: number, y: number, whack: boolean};
+      this.xcoord = data.x * 800;
+      this.ycoord = data.y * 500;
+
       this.x = data.x * 800 + "px";
       this.y = data.y * 500 + "px";
       this.whack = data.whack;
 
       if(this.whack) {
         console.log("X: " + this.x + ", Y: " + this.y);
+        if(this.xcoord >= 100 && this.ycoord >= 50 && this.xcoord < 200 && this.ycoord < 200) {
+          if(this.moles[0]) {
+            this.moles[0] = false;
+            ++this.score;
+          }
+        }
+        if(this.xcoord >= 350 && this.ycoord >= 50 && this.xcoord < 450 && this.ycoord < 200) {
+          if(this.moles[1]) {
+            this.moles[1] = false;
+            ++this.score;
+          }
+        }
+        if(this.xcoord >= 600 && this.ycoord >= 50 && this.xcoord < 700 && this.ycoord < 200) {
+          if(this.moles[2]) {
+            this.moles[2] = false;
+            ++this.score;
+          }
+        }
+        if(this.xcoord >= 100 && this.ycoord >= 300 && this.xcoord < 200 && this.ycoord < 450) {
+          if(this.moles[3]) {
+            this.moles[3] = false;
+            ++this.score;
+          }
+        }
+        if(this.xcoord >= 350 && this.ycoord >= 300 && this.xcoord < 450 && this.ycoord < 450) {
+          if(this.moles[4]) {
+            this.moles[4] = false;
+            ++this.score;
+          }
+        }
+        if(this.xcoord >= 600 && this.ycoord >= 300 && this.xcoord < 700 && this.ycoord < 450) {
+          if(this.moles[5]) {
+            this.moles[5] = false;
+            ++this.score;
+          }
+        }
       }
 
-      //50% chance for mole to spawn
-      if(Math.random() > 0.5) {
+      //25% chance for mole to spawn
+      if(Math.random() < 0.25) {
         this.moles[Math.floor(Math.random() * this.moles.length)] = true;
       }
     }))
